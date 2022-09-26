6d7f2e7b0869f71bf6a2da64ef774e48a7df621c
darshi88 <darshidixit123@gmail.com>
2022-09-26 17:21:35 +0530

    third commit

--- app.js
+++ app.js
@@ -1,66 +1,23 @@
-var express=require("express");
-var mongoose=require("mongoose");
-var bodyParser=require("body-parser");
-var app= express();
-app.set("view engine" ,"ejs");
+const express=require("express");
+const mongoose=require("mongoose");
+const bodyParser=require("body-parser");
+const todoRouter = require('../routes/Todo')
+const Todo =require('./model/Todo.model')
+
+app.use('./Todo',todoRouter)
+const app= express();
+// app.set("view engine" ,"ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-mongoose.connect("mongodb://localhost:27017/todolistDB")
+const url='mongodb://localhost:27017/todolistDB'
+mongoose.connect(url,{useNEWurlparser:true})
 const connection = mongoose.connection;
 
 connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
+    console.log("connected....");
 })
 
-const itemSchema=({
-    name: {type:String}
-}
-     
-);
 
-const item=mongoose.model("item,itemSchema")
-const item1=new Item({ name:"welcome to Itembuddies" });
-const item2=new Item({ name:"A" });
-const item3=new Item({ name:"B" });
-const d=[item1,item2,item3];
-Item.insertMany(d,function(err){
-    if(err){
-        console.log(err)
-    }else{
-        console.log("succesfully saved items to database");
-    }
-})
-app.get("/",function(req, res) {
-    Item.find({},function(err,f){
-        if(f.length===0){
-            Item.insertMany(d,function(err){
-                if(err){
-                    console.log(err)
-                }else{
-                    console.log("succesfully saved items to database");
-                }
-            });
-            res.redirect("/");
-        }
-    
-       else{ 
-        res.render("list", { newListItem: f });
-        }
-        
-    });
-});
-    
-
-app.post("/",function (req,res){
-     i=req.body.n;
-     const item = new Item({
-        name: i
-     })
-    item.save();
-    res.redirect("/");
-});
-
-
-app.listen(3000,function(){
-    console.log("listening on port 3000.");
+app.listen(9000,function(){
+    console.log('server started')
 })
