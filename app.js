feb961578324c70f683fa00bd77e38cfddc4c1a7
darshi88 <darshidixit123@gmail.com>
2022-11-08 15:11:52 +0530

    newfinal commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Todo =require('./model/todo.model')
 const app= express();
 app.use('./todo',router)
 
-// app.set("view engine" ,"ejs");
+app.set("view engine" ,"ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 const url='mongodb://localhost:27017/todolistDB'
@@ -16,6 +16,16 @@ const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("connected....");
 })
+
+var i1 = [];
+app.get("/" , function(req,res){
+    res.render("list",{newListItem:i1 });
+});
+app.post("/",function(req,res){
+     i=req.body.n;
+    i1.push(i);
+    res.redirect("/");
+});
 
 
 app.listen(9000,function(){
